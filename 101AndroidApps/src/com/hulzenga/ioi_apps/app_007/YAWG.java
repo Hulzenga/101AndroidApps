ef2fc0ec314999f149dbeb0f0602db2f1e43c621
Jouke Hulzenga <hulzenga@gmail.com>
2014-01-26 22:16:21 +0100

    Started App 7 (source is currently in other project, will be merged soon)

---
+++ 101AndroidApps/src/com/hulzenga/ioi_apps/app_007/YAWG.java
@@ -0,0 +1,12 @@
+package com.hulzenga.ioi_apps.app_007;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YAWG extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState);
+    }
+}
